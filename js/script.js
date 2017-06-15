c1620d55b4ce7a42d3a466bd82fc380d4ea02143
Thomas <thomasfaller@gmail.com>
2017-06-15 12:25:23 +0100

    almost completed

--- js/script.js
+++ js/script.js
@@ -1,17 +1,93 @@
-var content = document.getElementById('content');
-var ajaxRequest = new XMLHttpRequest();
-var url = 'https://wind-bow.glitch.me/twitch-api/users/quin69';
+var $url = 'https://wind-bow.glitch.me/twitch-api/';
 
-ajaxRequest.open("GET", url, true);
-responseType = 'json'
-ajaxRequest.onload = function() {
+                    // AJAX CALLS
 
-  var data = JSON.parse(this.response);
-  console.log(data);
-appendChild()
-
-
-  content.'<li class="list-group-item justify-content-between">Cras justo odio<span class="badge badge-default badge-pill">14</span></li>'
-
+function fccStatus() {                // Display the FCC channel status
+var html = 'FreeCodeCamp is currently Online!'
+$.ajax({
+  url: $url +  'streams/' + 'freecodecamp', // URL
+}).done(function(data) {
+  if (data.stream === null) {
+    html = 'FreeCodeCamp is currently Offline';
+  }
+  $('#fccstatus').html(html);
+});
 }
-ajaxRequest.send();
+
+function userStatus(user) {
+$.ajax({
+  url: $url + 'users/' + user + '/follows/channels/', // URL
+}).done(function(data) {
+  for (var i = 0; i < data.follows.length; i++ ) {
+      var path = data.follows[i].channel;     // assigning the JSON path to a variable to reduce code
+      var bgc = path.profile_banner_background_color;
+      var banner = path.profile_banner;
+      var link = path.url;
+      var username = path.display_name;
+
+      if (path.logo === null) {      // Logo check loop
+        var logo = 'http://www.mobilemag.com/wp-content/uploads/2012/09/twitch-tv-logo.png';
+        } else {
+        var logo = path.logo;
+        }
+
+      if (path.status === null) {      // Status check loop + Class added based on OFF/ON status
+        var status = '';
+        var $html = '<li class="list-group-item justify-content-between offline">';
+        } else {
+        var status = path.status;
+        var $html = '<li class="list-group-item justify-content-between online">';
+        }
+
+
+
+
+      console.log(data.follows[i].channel);
+
+
+      $html += '<span class="text-capitalize">' + username + '</span>';
+      $html += status;
+      $html += '<img src=' + logo + ' alt="" >'
+      $html += '</li>';
+      $('#content').append($html);
+
+    }
+  });
+}
+
+
+
+
+
+$(document).ready(function() {
+  fccStatus();
+  userStatus('freecodecamp');
+
+  $('#buttonAll').addClass('text-white');
+
+  $('#buttonOn').click(function() {
+    $('.text-white').toggleClass('text-white');
+    $(this).addClass('text-white');
+    $('.offline').hide();
+    $('.online').show();
+  });
+
+  $('#buttonOff').click(function() {
+    $('.text-white').toggleClass('text-white');
+    $(this).addClass('text-white');
+    $('.offline').show();
+    $('.online').hide();
+  });
+
+  $('#buttonAll').click(function() {
+    $('.text-white').toggleClass('text-white');
+    $(this).addClass('text-white');
+    $('.offline').show();
+    $('.online').show();
+
+  });
+
+
+  });
+
+// });
